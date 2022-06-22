694fe359945cf0d836681aa247334708aca4a919
Harry Bryant <harrybryant16@gmail.com>
2022-06-22 13:27:56 +0100

    Clauses returned as formulas so the grouping checks can take place

--- beispiele/IC3WithClauseGrouping.java
+++ beispiele/IC3WithClauseGrouping.java
@@ -365,53 +365,8 @@ public class IC3WithClauseGrouping {
         //Test formula to instantiate the new clause
         Formula clause = or(var(vars.get(0)), var(vars.get(1)));
 
-        //Loops through each element in the list of integers to create the clause
-        for (int i = 0; i < clauses.get(j).size(); i++) {
-
-            //Boolean to state whether the element is already a negative, and therefore should be stored as a negative
-            boolean negative = false;
-
-            //If the element read from the Dimacs file is less than 0, then the corresponding variable negative
-            if (clauses.get(j).get(i) < 0) {
-                negative = true;
-            }
-
-      /* Absolute value taken from the clause to avoid null pointer exceptions when loading the corresponding variable
-      from the ArrayList
-       */
-            int pos = (Math.abs(clauses.get(j).get(i))) - 1;
-
-      /*If formula is too be primed, the variables need to be from the second half of the list. This adds half the
-      number of variables to the position value of the element to convert it to it to the primed equivalent
-       */
-            if (isPrimed) {
-                pos += numberOfVars / 2;
-            }
-
-      /*The formula should add the negative version of the variable if one of the following conditions hold:
-      1. The variable is already negative and the formula itself shouldn't be negated
-      2. The variable is positive and the formula itself needs negating
-      Otherwise the positive version is added to the formula
-       */
-            if ((negative && !negateFormula) || (!negative && negateFormula)) {
-                //Negate variable
-                //If the clause is empty set the clause to the element, otherwise add the element via a logical or
-                if (i == 0) {
-                    clause = neg(var(vars.get(pos)));
-                } else {
-                    clause = or(clause, neg(var(vars.get(pos))));
-                }
-            } else {
-                //If the clause is empty set the clause to the element, otherwise add the element via a logical or
-                if (i == 0) {
-                    clause = var(vars.get(pos));
-                } else {
-                    clause = or(clause, var(vars.get(pos)));
-                }
-            }
-        }
         //The new clause is returned so it can be combined with any others to form the complete formula
-        return clause;
+        return clauseToFormula(clauses.get(j), negateFormula, isPrimed);
     }
 
     /**
@@ -836,7 +791,7 @@ public class IC3WithClauseGrouping {
 
             System.out.println("sat");
 
-            System.out.println(getClause(0,3));
+            System.out.println(getClause(0,1));
 
             //Variable to count the latest invariant that ¬s holds in
             int l = 0;
